8b5c4e66ee1e6cbd43f86f97a9438a87f2dede5b
Almas Meirmanov <almas1521@gmail.com>
2020-03-30 09:39:42 -0700

    #9 set up semantic-ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,25 +9,22 @@ import Restlogin from "./Restlogin"
 import Viewfreelancers from "./Viewfreelancers.js"
 import Jobpost from "./Jobpost.js"
 import FProfile from "./FProfile"
+import Home from "./Home"
 
 export default props => {
   return (
     <AuthProvider>
       <Router>
         <div className="box">
+          <Route exact path="/home" component={Home} />
           <Route exact path="/restlogin" component={Restlogin} />
-
           <Route exact path="/jobpost" component={Jobpost} />
           <Route exact path="/Viewfreelancers" component={Viewfreelancers} />
           <Route exact path="/rprofile" component={RProfile} />
           <Route exact path="/" component={Restregister} />
           <Route exact path="/fprofile" component={FProfile} />
-
           <Route exact path="/freelogin" component={Freelogin} />
-       
-          
-          
-//           <Route exact path="/restregister" component={Restregister} />
+          // <Route exact path="/restregister" component={Restregister} />
         </div>
       </Router>
     </AuthProvider>
