1b2daaa21fcc6e601236a47871d8011aed6fd319
Almas Meirmanov <almas1521@gmail.com>
2020-04-01 09:35:40 -0700

    #15 complete jobpost

--- client/src/components/Viewfreelancers.js
+++ client/src/components/Viewfreelancers.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react"
-// import { useAuth } from "react-auth"
+import { useAuth } from "react-auth"
 import { useJobs } from "../hooks"
 
 export default props => {
-  // const { profile } = useAuth()
-  const { viewpost, view } = useJobs()
-
-  // useEffect(() => {
-  //   viewpost(restname)
-  // }, [])
+  const { profile } = useAuth()
+  const { viewpost, view, getId } = useJobs(profile.username)
 
   return (
     <div className="Rprofile">
       <h1>SHIFT</h1>
-      <h3>{view.restname}</h3>
-      <h5>{view.jobdesc}</h5>
-      <h5>{view.pay}</h5>
+      {view.map(item => (
+        <div>
+          <h3>{item.restname}</h3>
+          <h5>{item.jobdesc}</h5>
+          <h5>${item.pay} per hour</h5>
+        </div>
+      ))}
     </div>
   )
 }
--- client/src/redux/ducks/viewjobposts/index.js
+++ client/src/redux/ducks/viewjobposts/index.js
@@ -1,38 +1,55 @@
 import { useDispatch, useSelector } from "react-redux"
 import axios from "axios"
+import { useEffect } from "react"
 
 const VIEWJOBPOSTS = "jobpost/VIEWJOBPOSTS"
-
+const GETID = "jobpost/GETID"
 const initialState = {
-  viewjobs: {}
+  viewjobs: [],
+  rest_id: ""
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case VIEWJOBPOSTS:
       return { ...state, viewjobs: action.payload }
+    case GETID:
+      return { ...state, rest_id: action.payload }
     default:
       return state
   }
 }
 
-function getJobs(restname, jobdesc, pay) {
+function getJobs(restID) {
   return dispatch => {
-    axios.get("/api/jobpost/", { restname, jobdesc, pay }).then(resp => {
-      const data = resp.data
-      console.log(data)
+    axios.get("/api/jobpost/" + restID).then(resp => {
+      console.log(resp.data)
       dispatch({
         type: VIEWJOBPOSTS,
-        payload: data
+        payload: resp.data
       })
     })
   }
 }
-
-export function useJobs() {
+function getUserId(username) {
+  return dispatch => {
+    axios.get("/api/getid/" + username).then(resp => {
+      dispatch({
+        type: GETID,
+        payload: resp.data
+      })
+    })
+  }
+}
+export function useJobs(username) {
   const dispatch = useDispatch()
-  const viewpost = (restname, jobdesc, pay) => getJobs(restname, jobdesc, pay)
+  //const viewpost = (restname, jobdesc, pay) => getJobs(restname, jobdesc, pay)
   const view = useSelector(appState => appState.viewpostState.viewjobs)
-
-  return { viewpost, view }
+  //const getId = username => dispatch(getUserId(username))
+  const restId = useSelector(appState => appState.viewpostState.rest_id)
+  useEffect(() => {
+    dispatch(getUserId(username))
+    dispatch(getJobs(restId))
+  }, [dispatch, restId])
+  return { view, restId }
 }
